09f55a936851e6cf017f3f95b56ff67365239d18
robotateme <jarwork884@gmail.com>
2023-05-23 08:03:03 +0300

    frontend Vue.js

---
+++ resources/js/components/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="container">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="collapse navbar-collapse">
+        <div class="navbar-nav" >
+        </div>
+      </div>
+    </nav>
+    <stats-component></stats-component>
+  </div>
+</template>
+<script>export default{}</script>
---
+++ resources/js/components/StatsComponent.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <b-table bordered hover striped :items="items" :fields="fields"
+            >
+
+            </b-table>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+          return {
+            fields: ['first_name', 'last_name', 'age'],
+            items: [
+              { age: 40, first_name: 'Dickerson', last_name: 'Macdonald' },
+              { age: 21, first_name: 'Larsen', last_name: 'Shaw' },
+              { age: 89, first_name: 'Geneva', last_name: 'Wilson' },
+              { age: 38, first_name: 'Jami', last_name: 'Carney' }
+            ]
+          }
+        },
+        mounted() {
+            axios.get();
+        }
+    }
+</script>
